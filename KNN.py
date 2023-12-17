7827e12c95377fece9536bd5c5a96bea908576a4
aninaeliza01 <aninaelizebeth2024a@mca.ajce.in>
2023-12-17 21:30:11 +0530

    commit3

--- KNN.py
+++ KNN.py
@@ -3,13 +3,23 @@ from sklearn.datasets import load_iris
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 
-iris=load_iris();
-x=iris.data
-y=iris.target
-x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
-clf=KNeighborsClassifier()
-clf.fit(x_train,y_train)
-v=clf.predict(x_test)
-print(v)
-print(accuracy_score(y_test,v))
-new_data=
+
+iris = load_iris()
+x = iris.data
+y = iris.target
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+clf = KNeighborsClassifier()
+clf.fit(x_train, y_train)
+
+test_predictions = clf.predict(x_test)
+test_accuracy = accuracy_score(y_test, test_predictions)
+print("Accuracy on test set:", test_accuracy)
+
+new_data = [[5.1, 3.5, 1.4, 0.2]]
+new_prediction = clf.predict(new_data)
+predicted_category = iris.target_names[new_prediction[0]]
+print("Prediction for new sample:", predicted_category)
+
+
